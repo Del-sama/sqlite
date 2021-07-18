3bdef6057e8610909e10256ddc303e52a568810c
Delores Diei <deloresdiei@gmail.com>
2021-07-18 10:25:53 +0200

    fixup repl

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("maining")
+	fmt.Println(repl(os.Stdin))
 }
--- repl.go
+++ repl.go
@@ -1,54 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
 
-type InputBuffer struct {
-	buffer       *string
-	bufferLength int
-	inputLength  int
+func repl(stdin io.Reader) error {
+	for {
+		printPrompt()
+		s := readInput(stdin)
+		validateInput(s)
+		os.Exit(0)
+	}
 }
-
-func (i *InputBuffer) newInputBuffer() {
-	inputBuffer := InputBuffer{}
-	inputBuffer.buffer = nil
-	inputBuffer.bufferLength = 0
-	inputBuffer.inputLength = 0
-}
-
 func printPrompt() {
 	fmt.Printf("db >")
 }
 
-func readInput(stdin io.Reader) (string, error) {
+func readInput(stdin io.Reader) string {
 	reader := bufio.NewReader(stdin)
-	return reader.ReadString('\n')
+	s, _ := reader.ReadString('\n')
+	return strings.TrimSpace(s)
 }
 
-int main(int argc, char* argv[]) {
-	InputBuffer* input_buffer = new_input_buffer();
-	while (true) {
-	  print_prompt();
-	  read_input(input_buffer);
-
-	  if (strcmp(input_buffer->buffer, ".exit") == 0) {
-		close_input_buffer(input_buffer);
-		exit(EXIT_SUCCESS);
-	  } else {
-		printf("Unrecognized command '%s'.\n", input_buffer->buffer);
-	  }
+func validateInput(input string) {
+	if len(input) < 1 {
+		fmt.Println("Error reading input")
+		os.Exit(1)
 	}
-  }
-
-
-func repl(argc int, argv *[]string) error {
-	inputBuffer := newInputBuffer()
-	for {
-		printPrompt()
-		s, err := readInput()
-		if err != nil {
-			return err
-		}
-
-
+	if input != ".exit" {
+		fmt.Printf("Unrecognized command %s", input)
 	}
 }
